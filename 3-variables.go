7d5eb6a37e3dfd4b79ed3eddcc0d956fe9ffb1ae
Matheus Alves Pereira <matttalves@gmail.com>
2022-09-14 22:34:48 -0300

    init

---
+++ 3-variables.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apply"
+	fmt.Println(f)
+
+	var g string
+	fmt.Println(g)
+
+	var h float64
+	fmt.Println(h)
+}

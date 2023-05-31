d369558b8f4629b80a035cc72d2a5395884a51ff
Matheus Alves Pereira <matttalves@gmail.com>
2023-05-31 19:34:22 -0300

    feat: WaitGroups

---
+++ 38-waitgroups.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func worker1(id int) {
+	fmt.Printf("Worker %d starting\n", id)
+	time.Sleep(time.Second)
+	fmt.Printf("Worker %d done\n", id)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
+		i := i
+		go func() {
+			defer wg.Done()
+			worker1(i)
+		}()
+	}
+
+	wg.Wait()
+}

5c64828b8a2e9e6da1c1592232981d76c27e68df
Matheus Alves Pereira <matttalves@gmail.com>
2023-05-31 18:27:16 -0300

    feat: non-blocking channel, closing channels, range channels, timers, tickers

---
+++ 36-tickers.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ticker := time.NewTicker(500 * time.Millisecond)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case t := <-ticker.C:
+				fmt.Println("tick at", t)
+			}
+		}
+	}()
+
+	time.Sleep(2 * time.Second)
+	ticker.Stop()
+	done <- true
+	fmt.Println("Ticker stopped")
+}

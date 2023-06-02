6cfbf0b8561079294ca73846add2ac095424cc26
Matheus Alves Pereira <matttalves@gmail.com>
2023-06-02 19:37:27 -0300

    feat: random numbers

---
+++ 58-random-numbers.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	p := fmt.Println
+
+	p(rand.Intn(100), ",")
+	p(rand.Intn(100))
+
+	p(rand.Float64())
+
+	p((rand.Float64()*5)+5, ",")
+	p((rand.Float64() * 5) + 5)
+
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	p(r1.Intn(100), ",")
+	p(r1.Intn(100))
+
+	s2 := rand.NewSource(42)
+	r2 := rand.New(s2)
+	p(r2.Intn(100), ",")
+	p(r2.Intn(100))
+
+	s3 := rand.NewSource(42)
+	r3 := rand.New(s3)
+	p(r3.Intn(100), ",")
+	p(r3.Intn(100))
+
+	var number int
+	tries := 0
+	for {
+		if number == 1 {
+			p("exiting for with", tries, "tries")
+			break
+		}
+
+		number = rand.Intn(100)
+		tries++
+	}
+}

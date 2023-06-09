e89bc8838e62c05d8feba2c72ed1be6f99116aee
Matheus Alves Pereira <matttalves@gmail.com>
2023-06-08 22:57:23 -0300

    feat: panic and recover

--- tour/basics/13-defer.go
+++ tour/basics/13-defer.go
@@ -17,10 +17,39 @@ func deferStack() {
 	fmt.Println("done")
 }
 
+func deferMutation() (i int) {
+	defer func() { i++ }()
+	return 1
+}
+
+func f() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in f", r)
+		}
+	}()
+	fmt.Println("Calling g.")
+	g(0)
+	fmt.Println("Returned normally from g.")
+}
+
+func g(i int) {
+	if i > 3 {
+		fmt.Println("Panicking!")
+		panic(fmt.Sprintf("%v", i))
+	}
+	defer fmt.Println("Defer in g", i)
+	fmt.Println("Printing in g", i)
+	g(i + 1)
+}
+
 func DeferExample() {
 	fmt.Println("a")
 	defer fmt.Println("b")
 	deferPrint()
 	deferStack()
 	fmt.Println("\nc")
+	fmt.Println("deferMutation value:", deferMutation())
+	f()
+	fmt.Println("Returned normally from f.")
 }

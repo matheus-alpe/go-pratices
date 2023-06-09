8431c80508501153cb70affda4c42c4ea157704c
Matheus Alves Pereira <matttalves@gmail.com>
2023-06-09 18:35:15 -0300

    feat: struct literals

--- tour/basics/15-structs.go
+++ tour/basics/15-structs.go
@@ -6,6 +6,13 @@ type Vertex struct {
 	x, y int
 }
 
+var (
+	v1 = Vertex{1, 2}
+	v2 = Vertex{x: 1}
+	v3 = Vertex{}
+	p1 = &Vertex{1, 2}
+)
+
 func StructExample() {
 	v := Vertex{1, 2}
 	fmt.Println(v)
@@ -15,4 +22,9 @@ func StructExample() {
 	p := &v
 	p.x = 1e9
 	fmt.Println(v)
+
+	fmt.Println(v1)
+	fmt.Println(v2)
+	fmt.Println(v3)
+	fmt.Println(p1)
 }
